aec6d300ea0396a26f6fff7ba810a5757a3881c6
Alessandro <AlessandroMarin80@gmail.com>
2018-02-17 00:13:43 -0500

    First commit

---
+++ resume/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.sites.requests import RequestSite
+from django.template import RequestContext
+
+from .models import Overview, PersonalInfo, Education, Job, Accomplishment, Skillset, Skill
+
+def index(request):
+    site_name = RequestSite(request).domain
+    personal_info = PersonalInfo.objects.all()[:1]
+    overview = Overview.objects.all()[:1]
+    education = Education.objects.all()
+    job_list = Job.objects.all()
+    skill_sets = Skillset.objects.all()
+
+    return render(request, 'resume/resume.html', {
+        'site_name': site_name,
+        'personal_info': personal_info,
+        'overview' : overview,
+        'job_list' : job_list,
+        'education' : education,
+        'skill_sets' : skill_sets,
+    })
+
+    '''return render_to_response('resume/resume.html', {
+                    'site_name': site_name,
+                    'personal_info': personal_info,
+                    'overview' : overview,
+                    'job_list' : job_list,
+                    'education' : education,
+                    'skill_sets' : skill_sets,
+             }, context_instance=RequestContext(request))'''
+
