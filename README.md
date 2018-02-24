ff5980157452aeae246b59286d42a02fbfe950b6
Alessandro <AlessandroMarin80@gmail.com>
2018-02-24 10:12:30 -0500

    Sat Feb 24 10:12:30 EST 2018

--- README.md
+++ README.md
@@ -5,42 +5,77 @@ Fire up a shell and create your resume with commands similar to the following on
 ```
 python3 manage.py shell
 
+import datetime
+
 from resume.models import PersonalInfo
-pi=PersonalInfo.objects.create('email': 'AlessandroMarin80@gmail.com', 'region': 'Massachusetts', 'region_shorthand': 'MA', 'first_name': 'Alessandro', 'locality': 'Boston', 'last_name': 'Marin')
+pi=PersonalInfo.objects.create(email='AlessandroMarin80@gmail.com', region='MA', first_name= 'Alessandro', locality='Boston', last_name='Marin')
 pi.linkedin='https://www.linkedin.com/in/alessandromarin80/'
+pi.github='https://github.com/aless80/'
+pi.sitename='my portfolio site'
+pi.site='www.myportfoliosite.com'
 pi.save()
 pi.__dict__  #print the fields
 
+from resume.models import Language
+l1=Language.objects.create(language='English',level='professional', personalinfo=pi)
+l2=Language.objects.create(language='Dutch',level='professional', personalinfo=pi)
+l2.ordering=2
+l1.save()
+l2.save()
+
 from resume.models import Overview
 over=Overview.objects.create(text='This is a test overview for this CV')
+over.save()
 
-from resume.models import Education
+from resume.models import Skillset
+from resume.models import Skill
 sks=Skillset.objects.create(name='Programming Languages')
-sks.save()
 s1=Skill.objects.create(name='Python',skillset=sks)
 s2=Skill.objects.create(name='Django',skillset=sks)
+s1.save()
+s2.save()
+sks.save()
 
-date=datetime.date(year=2018,month=2,day=19)
-date2=datetime.date(year=2018,month=2,day=20)
-j=Job.objects.create(company='MyCompany',start_date=date,completion_date=date2)
-j.company='my company'
-j.company_url='www.google.com'
+from resume.models import Job
+j=Job.objects.create(company='MyCompany')
+j.start_date=datetime.date(year=2018,month=2,day=19)
+j.end_date=datetime.date(year=2018,month=3,day=20)
+j.company='Best Co'
+j.company_url='https://www.google.com/search?q=best+co'
 j.title="CTO"
-j.description="some description of the job which goes here and will be displayed ok you get it"
+j.location='Boston, MA'
+j.description="Some description of the job which goes here and will be displayed ok you get it"
+j.save()
+
+j2=Job.objects.create(company='Booking.com')
+j2.start_date=datetime.date(year=2015,month=6,day=19)
+j2.end_date=datetime.date(year=2017,month=3,day=20)
+j2.company='Booking.com'
+j2.company_url='https://www.booking.com'
+j2.location='Amsterdam, the Netherlands'
+j2.title="Technician"
+j2.description="Describe the job here"
+j2.save()
 
 from resume.models import Accomplishment
 acc=Accomplishment.objects.create(order=2,job_id=1)
-acc.description="first accomplishement"
+acc.description="2nd accomplishement"
 acc.save()
 acc=Accomplishment.objects.create(order=3,job_id=1)
-acc.description="my second incomplete accomplishm"
+acc.description="my third incomplete accomplishm"
 acc.save()
 acc=Accomplishment.objects.create(order=1,job_id=1)
-acc.description="accomplishment there"
+acc.description="first accomplishment there"
 acc.save()
 
 from resume.models import Education
-Education.objects.create(name='University Somewhere',start_date=date,completion_date=date2)
+ed1=Education.objects.create(name='University of Somewhere')
+ed1.start_date=datetime.date(year=2010,month=2,day=19)
+ed1.end_date=datetime.date(year=2015,month=3,day=1)
+ed1.save()
+ed2=Education.objects.create(name='University of Somewherelse')
+ed2.start_date=datetime.date(year=2015,month=4,day=1)
+ed2.save()
 ```
 The available models are: 
 Overview
--- resume/models.py
+++ resume/models.py
@@ -13,25 +13,44 @@ class PersonalInfo(models.Model):
     middle_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255)
     locality = models.CharField(max_length=255, help_text="e.g. city such as Boston")
-    region = models.CharField(max_length=255, help_text="e.g. state such as Massachusetts",blank=True)
-    region_shorthand = models.CharField(max_length=64, help_text="e.g. shorthand (abbr), MA for Massachusetts")
+    region = models.CharField(max_length=255, help_text="e.g. MA or Italy",blank=True)
+    #region_shorthand = models.CharField(max_length=64, help_text="e.g. shorthand (abbr), MA for Massachusetts")
     email = models.EmailField(blank=True)
     linkedin = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    site = models.URLField(blank=True)
+    sitename  = models.CharField(max_length=255, default = site)
+    twittername = models.CharField(max_length=100, blank=True)
     class Meta:
         verbose_name_plural = "Personal Info"    
     def full_name(self):
         return " ".join([self.first_name, self.middle_name, self.last_name])    
+    def githubname(self):
+        if self.github:
+            return self.github.rsplit('/',maxsplit=1)[1]
+        else:
+            return None
     def __unicode__(self):
         return self.full_name()
+
+class Language(models.Model):
+    language = models.CharField(max_length=20,blank=False)
+    level = models.CharField(max_length=30,blank=False)
+    personalinfo = models.ForeignKey('PersonalInfo',on_delete=models.CASCADE)
+    ordering = models.IntegerField(default=1)
+    class Meta:
+        ordering = ['ordering','id']
+    def __unicode__(self):
+        return ''.join([self.PersonalInfo.full_name(), '-', self.language, '-', self.level])
 
 class Education(models.Model):
     name = models.CharField(max_length=250)
     location = models.CharField(max_length=250)
     school_url = models.URLField('School URL', blank=True)
-    start_date = models.DateField()
-    completion_date = models.DateField()
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
     summary = models.TextField(blank=True)
-    is_current = models.BooleanField(default=False)
+    #is_current = models.BooleanField(default=True)
     class Meta:
         verbose_name_plural = "Education"
     def edu_date_range(self):
@@ -40,17 +59,17 @@ class Education(models.Model):
     def full_start_date(self):
         return self.start_date.strftime("%Y-%m-%d")
     def full_end_date(self):
-        if (self.is_current == True):
+        if (self.end_date == None):
             return time.strftime("%Y-%m-%d", time.localtime())
         else:
-            return self.completion_date.strftime("%Y-%m-%d")
+            return self.end_date.strftime("%Y-%m-%d")
     def formatted_start_date(self):
         return self.start_date.strftime("%b %Y")
     def formatted_end_date(self):
-        if (self.is_current == True):
+        if (self.end_date == None):
             return "Current"
         else:
-            return self.completion_date.strftime("%b %Y")
+            return self.end_date.strftime("%b %Y")
     def __unicode__(self):
         return ' '.join([self.name, self.edu_date_range()])
 
@@ -61,32 +80,36 @@ class Job(models.Model):
     title = models.CharField(max_length=250)
     company_url = models.URLField('Company URL')
     description = models.TextField(blank=True)
-    start_date = models.DateField()
-    completion_date = models.DateField(blank=True)
+    start_date = models.DateField(blank=True,null=True)
+    end_date = models.DateField(blank=True,null=True)
     is_current = models.BooleanField(default=False)
     is_public = models.BooleanField(default=True)
     company_image = models.CharField(max_length=250, blank=True, 
         help_text='path to company image, local or otherwise')
     class Meta:
         db_table = 'jobs'
-        ordering = ['-completion_date','-start_date']        
+        ordering = ['-end_date','-start_date']        
     def job_date_range(self):
         return ''.join(['(', self.formatted_start_date(),'-', 
             self.formatted_end_date(), ')'])    
     def full_start_date(self):
+        if self.start_date is None:
+            return None
         return self.start_date.strftime("%Y-%m-%d")
     def full_end_date(self):
-        if (self.is_current == True):
+        if (self.is_current is True):
             return time.strftime("%Y-%m-%d", time.localtime())
         else:
-            return self.completion_date.strftime("%Y-%m-%d")
+            return self.end_date.strftime("%Y-%m-%d")
     def formatted_start_date(self):
-            return self.start_date.strftime("%b %Y")        
+        if self.start_date is None:
+            return None
+        return self.start_date.strftime("%b %Y")        
     def formatted_end_date(self):
-        if (self.is_current == True):
+        if self.is_current == True or self.end_date is None:
             return "Current"
         else:
-            return self.completion_date.strftime("%b %Y")
+            return self.end_date.strftime("%b %Y")
     def __unicode__(self):
         return ' '.join([self.company, self.job_date_range()])
 
@@ -94,10 +117,10 @@ class Accomplishment(models.Model):
     description = models.TextField()
     #job = models.ForeignKey(Job)
     job = models.ForeignKey('Job',on_delete=models.CASCADE)
-    order = models.IntegerField()
+    order = models.IntegerField(default=1)
     class Meta:
         db_table = 'accomplishments'
-        ordering = ['order']
+        ordering = ['order', 'id']
     def __unicode__(self):
         return ''.join([self.job.company, '-', self.description[0:50], '...'])
 
