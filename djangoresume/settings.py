69888a586c6b1179a1ffa27e3a946eba346d8966
Alessandro <AlessandroMarin80@gmail.com>
2018-03-06 11:04:40 -0500

    Tue Mar  6 11:04:40 EST 2018

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pdr@%#n+*#s)4d5rso^m#a@2b=ozbv3k6)z8^)4)-&k7#fc)%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','aless80.pythonanywhere.com']
 
 
 # Application definition
