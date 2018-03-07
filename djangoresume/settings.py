d0fc6ce3589092c779d1cca0ad5be6c951275580
Alessandro <AlessandroMarin80@gmail.com>
2018-03-06 21:54:25 -0500

    Tue Mar  6 21:54:25 EST 2018

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os 
 
+import sys
+RUNNING_DEVSERVER = (len(sys.argv) > 1 and sys.argv[1] == 'runserver')
+print("RUNNING_DEVSERVER: "+str(RUNNING_DEVSERVER))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pdr@%#n+*#s)4d5rso^m#a@2b=ozbv3k6)z8^)4)-&k7#fc)%i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','aless80.pythonanywhere.com']
 
